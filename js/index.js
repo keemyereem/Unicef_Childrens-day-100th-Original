7a888316f8f7e1c8d7251f5bec44b17821b051aa
keemyereem <bmongk0815@gmail.com>
2022-05-03 22:45:18 +0900

    0503_final

--- js/index.js
+++ js/index.js
@@ -19,10 +19,7 @@ var section1 = {
                 //스크롤 올릴때
             } else {
                 //스크롤 내릴때
-            }
-
-            
-            
+            } 
         });
     },
 
@@ -201,11 +198,12 @@ var s5Slider = {
         function s5Progress(index) {
             var count = $s5_slider.slick('getSlick').slideCount;
             console.log(count);
-            console.log('index : ' + index)
-            if (index == 0) {
-                index = index + 5;
-            }
-            var s5_calc = index * 20;
+            console.log('index : ' + index);
+            
+            // if (index == 0) {
+            //     index = index + 5;
+            // }
+            var s5_calc = (index + 1) * 20;
             $s5_progressBar
             .css('background-size', s5_calc + '% 100%')
             .attr('aria-valuenow', s5_calc );
@@ -222,6 +220,7 @@ var s5Slider = {
             slidesToScroll: 1,
             autoplay: false,
             // autoplaySpeed: 5000,
+            initialSlide : 0,
             infinite: true,
             swipeToSlide: true,
             cssEase: 'ease-out',
@@ -263,10 +262,7 @@ var s5Slider = {
     },
     clickEvent:function(){
         $(".section5 .slider_wrap .item").click(function() {
-            console.log($(this).index());
-            //$(this).toggleClass('on');
-            $(this).find('.basic').fadeToggle(300)
-            $(this).find('.hover').fadeToggle(300)
+            $(this).toggleClass('on');
         });
 
           
@@ -331,10 +327,11 @@ var section4 = {
 
 var s3Slider = {
     init:function(){
-        this.s3Slider();
+        this.s3Slider(); 
+        //this.s3Slider_extends();
     },
 
-    s3Slider:function(){
+    s3Slider:function(){        
         var $slide = $('.section3 .slider_wrap');
         var $nav = $('.section3 .slider_dots').find('li');
         var enableNav = true; //클릭하여 내비게이션 이동 허용 여부(슬라이드 동작 중 클릭되는 것을 방지)
@@ -345,23 +342,24 @@ var s3Slider = {
           $("#slide_paging").html('<b class="page">'+ (slick.currentSlide+1) +'</b> / ' + (slick.$dots[0].children.length));
         }).on('beforeChange', function(event, slick, currentSlide, nextSlide){ //슬라이드 변경 시 내비 및 페이징 변경
           //내비 변경
-          if(enableNav){
-            $nav.removeClass("on");
-            $nav.eq(nextSlide).addClass("on");
-            navStatus();
-          }
+        $nav.removeClass("on");
+        $nav.eq(nextSlide).addClass("on");
+        //   if(enableNav){
+            
+        //     navStatus();
+        //   }
           //페이징 변경
           if(!slick.$dots) return;
           var i = (nextSlide ? nextSlide : 0) + 1;
           $("#slide_paging").find(".page").text(i);
         });
       
-        function navStatus(){ //슬라이드 동작 중 내비클릭 방지
-          enableNav = false;
-          setTimeout(function() {
-            enableNav = true;
-          }, speed);
-        }
+        // function navStatus(){ //슬라이드 동작 중 내비클릭 방지
+        //   enableNav = false;
+        //   setTimeout(function() {
+        //     enableNav = true;
+        //   }, speed);
+        // }
       
         $nav.on("click", function(){ //내비 클릭시 해당 인덱스로 이동
           if(enableNav){
@@ -378,8 +376,8 @@ var s3Slider = {
         $('.section3 .slider_wrap').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed:4000,
+            autoplay: false,
+            autoplaySpeed:3000,
             infinite: true,
             swipeToSlide: true,
             cssEase: 'ease-out',
@@ -405,6 +403,7 @@ var s3Slider = {
                     }
                 }
             ]
+        
         }).on('afterChange', function(event, slick, currentSlide, nextSlide){ 
             console.log('slick index : ' + currentSlide);
             
@@ -467,13 +466,21 @@ var s3Slider = {
             draw(25, '#mov_graph05', '#ffc20e');
           }
 
+    },
+
+    s3Slider_extends:function() {
+        var swiper = new Swiper(".mySwiper", {
+            pagination: {
+              el: ".swiper-pagination",
+            },
+        });
     }
 };
 
 var scrollFadeUp = {
     init:function(){
         this.scrollEvent();
-        this.tweenmax();
+        //this.tweenmax();
     },
     scrollEvent:function(){
         $(window).on('scroll', function() {
@@ -538,26 +545,38 @@ var fullpage = {
         $('#fullpage').fullpage({
             'verticalCentered': false,
             'css3': true,
-            'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
+            // 'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
     
             'afterLoad': function(anchorLink, index){
-                if(index == 2){
-                    $('#iphone3, #iphone2, #iphone4').addClass('active');
+                if(index == 3){
+                    
                 }
             },
-    
+
             'onLeave': function(index, nextIndex, direction){
-                if (index == 3 && direction == 'down'){
-                    $('.fp-section').eq(index-1).removeClass('moveDown').addClass('moveUp');
+                if(nextIndex == 4 || nextIndex >= 8 || nextIndex == 6){
+                    $('.scroll, .logo').fadeOut();
+                } else {
+                    $('.scroll, .logo').fadeIn();
                 }
+                /*
                 else if(index == 3 && direction == 'up'){
                     $('.fp-section').eq(index-1).removeClass('moveUp').addClass('moveDown');
                 }
-    
-                $('#staticImg').toggleClass('active', (index == 2 && direction == 'down' ) || (index == 4 && direction == 'up'));
-                $('#staticImg').toggleClass('moveDown', nextIndex == 4);
-                $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
+                */
             }
         });
     },
 };
+
+function clip(){
+    var url = '';
+    var textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    url = window.document.location.href;
+    textarea.value = url;
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("URL이 클립보드에 복사되었습니다.")
+}
